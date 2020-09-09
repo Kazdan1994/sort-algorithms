48119f3cc5372279a131a2717b957ba3cec8f5b4
Kazdan1994 <arthurjacquemin2@gmail.com>
2020-09-09 10:51:40 +0200

    add tabs

---
+++ quicksort.js
@@ -0,0 +1,22 @@
+function partition(tab) {
+    
+}
+
+const quickSort = (tab) => {
+    let N = tab.length
+    let pivot = 0
+
+    if (N > 1) {
+        pivot = tab[N - 1]
+        partition(tab, )
+
+        return tab
+    }
+
+};
+
+const T = [8, 2, 9, 4, 1]
+
+const sortTab = quickSort(T)
+
+console.log(sortTab)
---
+++ tab.js
@@ -0,0 +1,4 @@
+for (let i = 0; i < 5000; i++) {
+    console.log((Math.floor(Math.random() * (5000 - 1 + 1)) + 1) + ',')
+}
+
